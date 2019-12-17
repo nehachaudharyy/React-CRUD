26dde01ff946c55fa2733a40105125cb096f1b83
nehachaudharyy <41499504+nehachaudharyy@users.noreply.github.com>
2019-12-17 18:02:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React , { Component } from 'react';
+import ProductItem from './ProductItem';
+import AddProduct from './AddProduct';
+import './App.css';
+const products = [
+  {
+    name: 'ipad',
+    price: 200
+  },
+  {
+    name: 'iphone',
+    price: 650
+  }
+];
+localStorage.setItem('products', JSON.stringify(products));
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        products : JSON.parse(localStorage.getItem('products'))
+      }  
+      this.onDelete = this.onDelete.bind(this);
+      this.onAdd = this.onAdd.bind(this);
+      this.onEditSubmit = this.onEditSubmit.bind(this);
+  }
+  componentWillMount() {
+    const products = this.getProducts();
+     this.setState({products});
+    }
+    getProducts() {
+       return this.state.products
+     
+    }
+    onAdd(name, price){
+      const products = this.getProducts();
+      products.push({
+        name,
+        price
+      });
+      this.setState({ products});
+    }
+    onDelete(name){
+        //
+        const products = this.getProducts();
+        const filteredProducts = products.filter(product => {
+          return product.name !== name;
+        })
+        console.log(filteredProducts);
+        this.setState({ products : filteredProducts});
+    }
+    onEditSubmit(name,price, originalName){
+      console.log(name,price)
+      let products = this.getProducts();
+      products = products.map(product => {
+        if (product.name === originalName){
+          product.name = name;
+          product.price = price;
+        }
+        return product;
+      });
+      this.setState({products});
+    }
+  render() {
+    return(
+        <div className='App'>
+          <h1>Products Manager</h1>
+
+          <AddProduct onAdd = {this.onAdd}/>
+          {
+            this.state.products.map(product => {
+              return (
+                < ProductItem
+                 key = {product.name}
+                name = {product.name}
+                price = {product.price}
+                onDelete = {this.onDelete}
+                onEditSubmit = {this.onEditSubmit}
+                />
+              );
+            })
+          }
+        </div>
+    );
+  }
+}
+
+export default App;
